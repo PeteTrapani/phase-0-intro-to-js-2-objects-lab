2c3c358ba33f0dca7cd84ca6f27c5770b2033262
Pete Trapani <pete.trapani91@gmail.com>
2022-08-15 13:26:34 -0500

    commit tree 0b17ac7ffd338c48f62558b744fb9d413aa70a50

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Pete',
+    streetAddress: 'Longview',
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+function  deleteFromEmployeeByKey(employee, key) {
+    const newDel = { ...employee };
+    delete newDel[key]
+    return newDel;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
